e5b2ab96cf4651201d4442a5dcacf2860a1f0ca6
unknown <shilra066@gmail.com>
2025-06-05 02:32:52 +0700

    Pseudocode

--- findminmax.go
+++ findminmax.go
@@ -1,38 +1,39 @@
 package main
+
 import "fmt"
 
 const NMAX int = 100
 
 type tabInt [NMAX]int
 
-func main(){
+func main() {
 	var a tabInt
 	var i, ndata int
-	
+
 	fmt.Scan(&ndata)
 	for i = 0; i < ndata; i++ {
 		fmt.Scan(&a[i])
 	}
-	fmt.Println("min =",min(a, ndata))
-	fmt.Println("max =",max(a, ndata))
+	fmt.Println("min =", min(a, ndata))
+	fmt.Println("max =", max(a, ndata))
 }
 
-func min(a tabInt, n int)int {
+func min(a tabInt, n int) int {
 	var i, min int
 	min = 0
-	for i = 1; i < n; i++{
-		if a[min] > a[i]{
+	for i = 1; i < n; i++ {
+		if a[min] > a[i] {
 			min = i
 		}
 	}
 	return a[min]
 }
 
-func max(a tabInt, n int)int {
+func max(a tabInt, n int) int {
 	var i, max int
 	max = 0
-	for i = 1; i < n; i++{
-		if a[max] < a[i]{
+	for i = 1; i < n; i++ {
+		if a[max] < a[i] {
 			max = i
 		}
 	}
--- sequentialSearch.go
+++ sequentialSearch.go
@@ -1,14 +1,15 @@
 package main
+
 import "fmt"
 
 const NMAX int = 100
 
 type tabInt [NMAX]int
 
-func main(){
+func main() {
 	var a tabInt
 	var i, ndata, cari int
-	
+
 	fmt.Scan(&ndata)
 	for i = 0; i < ndata; i++ {
 		fmt.Scan(&a[i])
@@ -17,9 +18,9 @@ func main(){
 	fmt.Println(seqsearch(a, ndata, cari))
 }
 
-func seqsearch(a tabInt, n, cari int)bool{
+func seqsearch(a tabInt, n, cari int) bool {
 	var i int
-	
+
 	for i < n {
 		if a[i] == cari {
 			return true
